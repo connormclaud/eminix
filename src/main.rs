b11a4252b562cca2d6a74ec7834eebe8c758da95
Vitali Vinahradski <vitali@vinahradski.de>
2026-08-14 23:03:40 +0200

    feat(eee-002): add crossterm and raw-mode skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use crossterm::{terminal::disable_raw_mode, terminal::enable_raw_mode};
 fn main() {
+    enable_raw_mode().unwrap();
     println!("Hello, world!");
+    disable_raw_mode().unwrap();
 }
